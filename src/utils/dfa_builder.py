e72dd9a273db278ef58b29661a5defa78f356629
beyazit-yalcinkaya <beyazit.yalcinkaya@gmail.com>
2024-04-11 19:00:36 -0700

    Fix minor issue

--- src/utils/dfa_builder.py
+++ src/utils/dfa_builder.py
@@ -20,38 +20,42 @@ class DFABuilder(object):
     def __ring_key__(self):
         return "DFABuilder"
 
-    def __call__(self, dfa_int_seq, library="dgl"):
-        return self._to_graph(dfa_int_seq, library)
+    def __call__(self, dfa_int_seq):
+        return self._to_graph(dfa_int_seq)
 
     @ring.lru(maxsize=1000000)
-    def _to_graph(self, dfa_int_seq, library="dgl"):
+    def _to_graph(self, dfa_int_seq):
         l = dfa_int_seq.shape[0]
         dfa_goal_int_seq = dfa_int_seq.reshape(self.dfa_n_conjunctions, self.dfa_n_disjunctions, l//(self.dfa_n_conjunctions*self.dfa_n_disjunctions))
         nxg_goal = []
         nxg_goal_or_nodes = []
+        rename_goal = []
         for i, dfa_clause_int_seq in enumerate(dfa_goal_int_seq):
             nxg_clause = []
             nxg_init_nodes = []
+            rename_clause = []
             for j, dfa_int_seq in enumerate(dfa_clause_int_seq):
                 dfa_int_str = "".join(str(int(i)) for i in dfa_int_seq.tolist())
                 dfa_int = int(dfa_int_str)
                 if dfa_int > 0:
-                    nxg, init_node = self.dfa_int2nxg(dfa_int, str(i) + "_" + str(j) + "_")
+                    nxg, init_node = self.dfa_int2nxg(dfa_int)
                     nxg_clause.append(nxg)
-                    nxg_init_nodes.append(init_node)
+                    rename_clause.append(str(j) + "_")
+                    nxg_init_nodes.append(str(j) + "_" + init_node)
 
             if nxg_clause != []:
-                composed_nxg_clause = nx.union_all(nxg_clause)
-                or_node = str(i) + "_OR"
+                composed_nxg_clause = nx.union_all(nxg_clause, rename=rename_clause)
+                or_node = "OR"
                 composed_nxg_clause.add_node(or_node, feat=np.array([[0.0] * FEATURE_SIZE]))
                 composed_nxg_clause.nodes[or_node]["feat"][0][feature_inds["OR"]] = 1.0
                 for nxg_init_node in nxg_init_nodes:
                     composed_nxg_clause.add_edge(nxg_init_node, or_node, type=edge_types["OR"])
                 nxg_goal.append(composed_nxg_clause)
-                nxg_goal_or_nodes.append(or_node)
+                rename_goal.append(str(i) + "_")
+                nxg_goal_or_nodes.append(str(i) + "_" + or_node)
 
         if nxg_goal != []:
-            composed_nxg_goal = nx.union_all(nxg_goal)
+            composed_nxg_goal = nx.union_all(nxg_goal, rename=rename_goal)
         else:
             composed_nxg_goal = nx.DiGraph()
 
@@ -68,9 +72,6 @@ class DFABuilder(object):
             composed_nxg_goal.add_edge(node, node, type=edge_types["self"])
         
         nxg = composed_nxg_goal
-
-        if (library == "networkx"):
-            return nxg
 
         return self._get_dgl_graph(nxg)
 
@@ -96,14 +97,14 @@ class DFABuilder(object):
 
         return g
 
-    def dfa_to_formatted_nxg(self, dfa, node_name_prefix):
+    def dfa_to_formatted_nxg(self, dfa):
 
         nxg = nx.DiGraph()
         new_node_name_counter = 0
         new_node_name_base_str = "temp_"
 
         for s in dfa.states():
-            start = node_name_prefix + str(s)
+            start = str(s)
             nxg.add_node(start)
             nxg.nodes[start]["feat"] = np.array([[0.0] * FEATURE_SIZE])
             nxg.nodes[start]["feat"][0][feature_inds["normal"]] = 1.0
@@ -116,27 +117,27 @@ class DFABuilder(object):
                 e = dfa._transition(s, a)
                 if s == e:
                     continue # We define self loops later when composing graphs
-                end = node_name_prefix + str(e)
+                end = str(e)
                 if end not in embeddings.keys():
                     embeddings[end] = np.zeros(FEATURE_SIZE)
                     embeddings[end][feature_inds["temp"]] = 1.0 # Since it is a temp node
                 embeddings[end][self.propositions.index(a)] = 1.0
             for end in embeddings.keys():
-                new_node_name = node_name_prefix + new_node_name_base_str + str(new_node_name_counter)
+                new_node_name = new_node_name_base_str + str(new_node_name_counter)
                 new_node_name_counter += 1
                 nxg.add_node(new_node_name, feat=np.array(np.array([embeddings[end]])))
                 nxg.add_edge(new_node_name, start, type=edge_types["temp-to-normal"])
                 nxg.add_edge(end, new_node_name, type=edge_types["normal-to-temp"])
 
-        init_node = node_name_prefix + str(dfa.start)
+        init_node = str(dfa.start)
         nxg.nodes[init_node]["feat"][0][feature_inds["init"]] = 1.0
 
         return nxg, init_node
 
     @ring.lru(maxsize=1000000)
-    def dfa_int2nxg(self, dfa_int, node_name_prefix):
+    def dfa_int2nxg(self, dfa_int):
         dfa = DFA.from_int(dfa_int, self.propositions)
-        nxg, init_node = self.dfa_to_formatted_nxg(dfa, node_name_prefix)
+        nxg, init_node = self.dfa_to_formatted_nxg(dfa)
         return nxg, init_node
 
 def draw(G, formula):
